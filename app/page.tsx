7b2251a8d44213bd9fa724de2fc941ccea95ae00
Baldo <barrosoub@protonmail.com>
2025-01-28 13:38:42 -0600

    our project was initialized

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h3>Welcome</h3>
+      <button>Login</button>
+      <button>Logout</button>
+      
+    </div>
+  )
+}
+
+export default page
